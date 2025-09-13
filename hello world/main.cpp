9e605eb0da36930127c8fa343683b310385161ce
Dandadan <KanzArtem11@mail.ru>
2025-09-13 10:16:45 +0300

    Initial Commit

---
+++ hello world/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hello world
+//
+//  Created by Артемий Канзюба on 13.09.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
